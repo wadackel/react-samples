a7a318eb032185628e15d6a00b8fa56a1ba07113
tsuyoshiwada <v23.b.wada@gmail.com>
2016-04-27 01:17:37 +0900

    Add redux-logger

--- ssr/src/store/configureStore.js
+++ ssr/src/store/configureStore.js
@@ -2,18 +2,22 @@
 
 import {createStore, combineReducers, compose, applyMiddleware} from "redux"
 import {routerReducer, routerMiddleware} from "react-router-redux"
+import createLogger from "redux-logger"
 
 export default function configureStore(history, initialState) {
   const reducer = combineReducers({
     routing: routerReducer
   });
 
+  const logger = createLogger();
+
   const store = createStore(
     reducer,
     initialState,
     compose(
       applyMiddleware(
-        routerMiddleware(history)
+        routerMiddleware(history),
+        logger
       )
     )
   );
