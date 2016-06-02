1bce069b06e0c405f6fbddc03e036b4bf4c652a3
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-02 19:59:16 +0900

    Add material-ui

--- image-editor/src/components/app.js
+++ image-editor/src/components/app.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+import AppBar from "material-ui/AppBar";
+import IconButton from "material-ui/IconButton";
+import IconMenu from "material-ui/IconMenu";
+import MenuItem from "material-ui/MenuItem";
+import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
+import NavigationClose from "material-ui/svg-icons/navigation/close";
+import RotateLeftIcon from "material-ui/svg-icons/image/rotate-left";
+import RotateRightIcon from "material-ui/svg-icons/image/rotate-right";
 import ImageEditor from "./image-editor";
+
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
-    return <ImageEditor width="500" height="500" image="./images/screenshot.png" />;
+    return (
+      <div style={{width: "100%", height: "100%"}}>
+        <AppBar
+          title="ImageEditor"
+          iconElementLeft={<span />}
+          iconElementRight={
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              targetOrigin={{horizontal: "right", vertical: "top"}}
+              anchorOrigin={{horizontal: "right", vertical: "top"}}
+            >
+              <MenuItem primaryText="45°" leftIcon={<RotateRightIcon />} />
+              <MenuItem primaryText="-45°" leftIcon={<RotateLeftIcon />} />
+            </IconMenu>
+          }
+        />
+        <ImageEditor width="500" height="500" image="./images/screenshot.png" />
+      </div>
+    );
+    return ;
   }
 }
--- image-editor/src/entry.js
+++ image-editor/src/entry.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import App from "./components/app";
 
+injectTapEventPlugin();
+
+const darkMuiTheme = getMuiTheme(darkBaseTheme);
+
 render(
-  <App />,
+  <MuiThemeProvider muiTheme={darkMuiTheme}>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById("app")
 );
