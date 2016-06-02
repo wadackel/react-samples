52b9dd005c4e1ee529008755ef0add647f29ff65
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-02 15:02:57 +0900

    Initial imageEditor

---
+++ image-editor/src/components/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ImageEditor from "./image-editor";
+
+export default class App extends Component {
+  render() {
+    return <ImageEditor />;
+  }
+}
---
+++ image-editor/src/components/image-editor.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class ImageEditor extends Component {
+  render() {
+    return (
+      <div>ImageEditor</div>
+    );
+  }
+}
