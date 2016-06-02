eb40e79cd1bb08587985ef99824e9474001baafb
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-02 15:38:28 +0900

    wip image-editor

--- image-editor/src/components/app.js
+++ image-editor/src/components/app.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import ImageEditor from "./image-editor";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <ImageEditor />;
+    return <ImageEditor width="500" height="500" image="./images/screenshot.png" />;
   }
 }
--- image-editor/src/components/image-editor.js
+++ image-editor/src/components/image-editor.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 
 export default class ImageEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { width, height, image } = this.props;
+    const { canvas } = this.refs;
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+
+    img.addEventListener("load", () => {
+      ctx.drawImage(img, 0, 0);
+    }, false);
+
+    img.src = image;
+  }
+
   render() {
-    return (
-      <div>ImageEditor</div>
-    );
+    const { width, height } = this.props;
+    return <canvas ref="canvas" width={width} height={height}></canvas>;
   }
 }
