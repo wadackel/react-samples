7b766ec5adbcc1288ebc73d4a3df89d68097607f
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-04 09:59:09 +0900

    Change images

--- image-viewer/src/constants/images.js
+++ image-viewer/src/constants/images.js
@@ -1,2 +1,2 @@
-export const IMAGE1 = "./images/image1.png";
-export const IMAGE2 = "./images/image2.png";
+export const IMAGE1 = "./images/image1.jpg";
+export const IMAGE2 = "./images/image2.jpg";
